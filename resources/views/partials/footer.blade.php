b09f83216cd8c355e5f51e4d9a21f374489054c7
brizer <rabriceno@brizerconsulting.com>
2017-05-17 16:29:02 -0400

    Eliminando comentarios en app.blade - Cambiando texto en el footer - cambiando imagen de fondo de la pagina login

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="clearfix">
     <div class="pull-right">
-       Desarrollado <i class="fa fa-heart text-danger"></i> por <a href="http://brizerconsulting.com/" target="_blank">BrizerConsulting.com</a>
+       Desarrollado por <a href="http://brizerconsulting.com/" target="_blank">BrizerConsulting.com</a>
     </div>
     <div class="pull-left">
-      &copy;  {{ date('Y') }}  GRUPO Atiempo - Todos los derechos reservados
+      &copy;  {{ date('Y') }}  Corporacion Atiempo - Todos los derechos reservados
     </div>
 </footer>
