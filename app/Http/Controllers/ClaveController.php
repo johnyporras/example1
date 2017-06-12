be1b8c926546ce47e6396954c860a2302d3d0a81
johny <jhonnyporras@gmail.com>
2017-06-12 17:57:19 -0430

    corrección en la búsqueda de afiliados

--- app/Http/Controllers/ClaveController.php
+++ app/Http/Controllers/ClaveController.php
@@ -205,7 +205,8 @@ class ClaveController extends Controller{
         if($user->type == 3){ // PROVEEDOR
             $coberturas = DB::table('ac_cuenta')
                 ->where([['codigo_cuenta', '=', $beneficiario['contrato']]])
-                ->join('ac_planes_extranet', 'ac_planes_extranet.codigo_plan',"=", 'ac_cuenta.id_plan')
+                ->join('ac_cuentaplan', 'ac_cuentaplan.id_cuenta',"=", 'ac_cuenta.id')
+                ->join('ac_planes_extranet', 'ac_planes_extranet.id',"=", 'ac_cuentaplan.id_plan')
                 ->join('ac_cobertura_extranet', 'ac_cobertura_extranet.id_plan',"=", 'ac_planes_extranet.codigo_plan')
                 ->join('ac_procedimientos_medicos', function($join){
                         $join->on('ac_procedimientos_medicos.codigo_examen',"=", 'ac_cobertura_extranet.id_procedimiento')
@@ -227,7 +228,8 @@ class ClaveController extends Controller{
         }else{
             $coberturas = DB::table('ac_cuenta')
                 ->where([['codigo_cuenta', '=', $beneficiario['contrato']]])
-                ->join('ac_planes_extranet', 'ac_planes_extranet.codigo_plan',"=", 'ac_cuenta.id_plan')
+                ->join('ac_cuentaplan', 'ac_cuentaplan.id_cuenta',"=", 'ac_cuenta.id')
+                ->join('ac_planes_extranet', 'ac_planes_extranet.id',"=", 'ac_cuentaplan.id_plan')
                 ->join('ac_cobertura_extranet', 'ac_cobertura_extranet.id_plan',"=", 'ac_planes_extranet.codigo_plan')
                 ->join('ac_procedimientos_medicos', function($join){
                         $join->on('ac_procedimientos_medicos.codigo_examen',"=", 'ac_cobertura_extranet.id_procedimiento')
