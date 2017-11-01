dac59d54e2f386dc53b55cb552460124bc3b24f3
brizer <rabriceno@brizerconsulting.com>
2017-11-01 16:50:48 -0400

    generando codigo de membresia con compra en tienda

--- app/Http/Controllers/WebHookController.php
+++ app/Http/Controllers/WebHookController.php
@@ -65,7 +65,57 @@ class WebHookController extends Controller
 
         }elseif ($action === "action.woocommerce_order_status_completed"){
           //Payment completed - update order and send code
+          if($request->action ==="woocommerce_order_status_completed"){
+            //Get order products
+            $order = OrdenesWeb::where('id_orden',$request->arg)->get();
+            foreach($order as $prod){
+              // get product by code
+              if ($prod->producto == "AT-90" || $prod->producto == "AT-40") {
+                  // Product a-card / a-member
+                  $tplan = ($prod->producto == "AT-90")?'A-CARD':'A-MEMBER';
+                  // Genrate card code
 
+                  $val = 0;
+                  $count = 0;
+                  $tarjeta = 1;
+                  //creating new code
+                  while($tarjeta !== null){
+                    $value1 = rand(11111,55555);
+                    $value2 = rand(66666,99999);
+                    if($prod->producto == "AT-40"){
+                      //validar pais***
+                      echo $codigo = '4005803001'.$value1.$value2;
+                    }else{
+                      //validar pais***
+                      $codigo = '9005803001'.$value1.$value2;
+                    }
+
+                    //encript generated code
+                    $crypt = Tarjeta::cryptCode($codigo);
+                    //validate it does't exist in DB
+                    $tarjeta = Tarjeta::where('codigo_tarjeta',$crypt)->first();
+                    if($tarjeta === null){
+                      $val = 1;
+                      //store code in db
+                      $Tarjeta = Tarjeta::create([
+                                        'codigo_tarjeta' => $crypt,
+                                        'activada'         => 'N'
+                                    ]);
+                        $prod->codigo = $codigo;
+                        $prod->save();
+                    }else{
+                      $val = 0;
+                    }
+                    echo $count++;
+                  }
+
+              } else {
+                  // product a-doctor
+                  $tplan = 'A-DOCTOR';
+              }
+            }
+
+          }
         }else{
           //Response
           return response()->json('success', 240);
